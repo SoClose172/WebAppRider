c53c015e312bdb3c7da3174bfcda73451231a8ae
Rewer172 <privetdiablo3@mail.ru>
2018-11-22 01:26:46 +0300

    WebAppRider

---
+++ WebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        
+        [Required]
+        [Display(Name = "Год рождения")]
+        public int Year { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        
+        [Required]
+        [Compare("Password", ErrorMessage = "Not Confirm")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string PasswordConfirm { get; set; }
+        
+    }
+}
